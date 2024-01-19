084e59f651c684c929c6b58e4adcc78131b1c17f
hmdn123 <sahal.hamda.3@gmail.com>
2024-01-20 00:00:44 +0700

    GANTI COPYRIGHT

--- resources/views/Template/app.blade.php
+++ resources/views/Template/app.blade.php
@@ -161,7 +161,7 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>AEY Creative</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>HMDN</span></strong>. All Rights Reserved
     </div>
   </footer><!-- End Footer -->
 
